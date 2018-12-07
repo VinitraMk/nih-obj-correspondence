fda0e75c4c493ec8534884a5a928f0e2710b4ce0
VinitraMk <vinitramk@gmail.com>
2018-12-08 03:59:24 +0530

    updating train and pretrained scripts, adding test script

---
+++ test.py
@@ -0,0 +1,224 @@
+import sys
+import os
+import time
+import pandas as pd
+import numpy as np
+import pickle
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset,DataLoader
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+from sklearn.metrics import roc_auc_score
+import torchvision.datasets as datasets
+from PIL import Image
+import csv
+import cv2
+import imageio
+from sklearn.metrics import accuracy_score,jaccard_similarity_score
+
+class AlexNet(nn.Module):
+
+    def __init__(self,num_classes=15):
+        super(AlexNet,self).__init__()
+        self.features = nn.Sequential(
+                nn.Conv2d(3,64,kernel_size=11,stride=4,padding=2),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(kernel_size=3,stride=2),
+                nn.Conv2d(64,192,kernel_size=5,padding=2),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(kernel_size=3,stride=2),
+                nn.Conv2d(192,384,kernel_size=3,padding=1),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(384,256,kernel_size=3,padding=1),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(256,256,kernel_size=3,padding=1),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(kernel_size=2,stride=2),
+            )
+        self.classifier=nn.Sequential(
+                nn.Dropout(),
+                nn.Linear(256*6*6,4096),
+                nn.ReLU(inplace=True),
+                nn.Dropout(),
+                nn.Linear(4096,4096),
+                nn.ReLU(inplace=True),
+                nn.Linear(4096,num_classes),
+            )
+
+    def forward(self,x):
+        x=self.features(x)
+        x=x.view(x.size(0),256*6*6)
+        x=self.classifier(x)
+        return x
+
+class ChestXRayDataSet(Dataset):
+    def __init__(self,data_path,label_path,transform=None):
+        self.X=np.uint8(np.load(data_path)*224*224)
+        with open(label_path,"rb") as f:
+            self.y=pickle.load(f)
+        sub_bool=(self.y.sum(axis=1)!=0)
+        #print('sub_bool',sub_bool)
+        self.y=self.y[sub_bool,:]
+        self.X=self.X[sub_bool,:]
+        self.transform=transform
+
+    def __len__(self):
+        return len(self.y)
+
+    def __getitem__(self,index):
+        item=self.X[index]
+        label=self.y[index]
+
+        if self.transform:
+            item=self.transform(item)
+        return item,torch.from_numpy(label).type(torch.FloatTensor)
+
+def old_method():
+    dataset=ChestXRayDataSet(data_files,label_file,
+        transform=transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.RandomCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485,0.456,0.406],[0.229,0.224,0.224])
+            ]))
+    data_loader=DataLoader(dataset=dataset,batch_size=1,shuffle=True)
+
+    gt=torch.FloatTensor()
+    pred=torch.FloatTensor()
+    i=1
+    print(len(data_loader))
+
+    if torch.no_grad():
+        for (image,target) in data_loader:
+            image=Variable(image.view(-1,1,224,224),0)
+            output=model(image)
+            sigm=torch.nn.Sigmoid()
+            probs=sigm(output)
+            print(probs)
+            indices=np.argwhere(probs>=0.067)
+
+def image_loader(image_name):
+    imsize=224
+    normalize = transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+        )
+    transform = transforms.Compose([transforms.ToPILImage(),
+    	transforms.Resize(imsize),
+        transforms.RandomCrop(imsize),
+    transforms.ToTensor(),
+    normalize])
+    #current_X=Image.open(image_name)
+    current_X=imageio.imread(image_name)
+    #print(len(current_X))
+    if current_X.shape!=(1024,1024):
+        current_X=current_X[:,:,0]
+
+    x_data=[]
+    x_data.append(np.array(current_X).reshape(1024,1024,1))
+    image=np.tile(x_data[0],3)
+    #print(type(image))
+    image=transform(image)
+    image=Variable(image,requires_grad=True)
+    return image
+
+def compute_AUCs(gt, pred):
+
+    AUROCs = []
+    gt_np = gt.cpu().numpy()
+    pred_np = pred.cpu().numpy()
+    for i in range(N_CLASSES):
+        AUROCs.append(roc_auc_score(gt_np[:, i], pred_np[:, i]))
+    return AUROCs
+
+def main():
+    start_time=time.time()
+    cwd=sys.argv[1]#'C:\\Users\\mushu\\Desktop\\PythonTest\\'
+    data_fol_name=sys.argv[2]#'test_5'
+    output_file_name=sys.argv[3]#'out.csv'
+    cwd = os.path.join(cwd,data_fol_name)
+    #print('\nCurrent working directory:',cwd)
+    N_CLASSES=15
+    model=AlexNet(N_CLASSES)
+    model_file_name='C:\\Users\\mushu\\Desktop\\PythonTest\\AlexNet_Epoch_4.pkl'
+    checkpoint=torch.load(model_file_name,map_location=lambda storage, loc: storage)
+    model.load_state_dict(checkpoint["state_dict"])
+    model.eval()
+    data_files=sorted([x for x in os.listdir(cwd) if x.endswith('.png')])
+    label_file=[x for x in  os.listdir(cwd) if x.endswith('.txt')][0]
+    label_file=os.path.join(cwd,label_file)
+    fp=open(label_file,"r")
+    labels = sorted([x for x in fp.readlines() if x!=""])
+    #print('\n\nNo of files to be read:',len(data_files))
+    #print('\n')
+    CLASS_NAMES = sorted(['Atelectasis', 'Cardiomegaly','Effusion', 'Infiltration','Mass','Nodule', 'Pneumonia', 'Pneumothorax','Consolidation','Edema','Emphysema','Fibrosis','Pleural_Thickening','Hernia','No Finding'])
+    class_map={}
+    i=0
+    for s in CLASS_NAMES:
+        class_map[s]=i
+        i=i+1
+
+    #thresholds = np.load('thresholds.npy')
+    #print(thresholds)
+    fp=open(output_file_name,'w')
+    csv_writer=csv.writer(fp)
+    file_header=['File Name','Finding Labels']
+    csv_writer.writerow(file_header)
+    avg_acc=0.0
+    all_targets=[]
+    all_bin_preds=[]
+
+    for i in range(len(labels)):
+        fname=labels[i].split(":")[0]
+        target=labels[i].split(":")[1].rstrip()
+
+        bin_target=[0]*N_CLASSES
+        bin_pred_list=[0]*N_CLASSES
+
+
+        gtlabels=[]
+        if("|" in target):
+            gtlabels=target.split("|")
+        else:
+            gtlabels.append(target)
+        for s in gtlabels:
+            bin_target[class_map[s]]=1
+
+        #all_targets.append(bin_target)
+
+        if(fname==data_files[i]):
+            impath=os.path.join(cwd,fname)
+            img_tensor=image_loader(impath)
+            img_tensor=Variable(img_tensor.view(-1,3,224,224),0)
+            if torch.no_grad():
+                output=model(img_tensor)
+                sigm=torch.nn.Sigmoid()
+                probs=sigm(output)[0]
+                #print(probs)
+                indices=np.argwhere(probs>=0.066)[0]
+                pred_label_list=[]
+                for j in indices.data:
+                    pred_label_list.append(CLASS_NAMES[j])
+                    bin_pred_list[class_map[CLASS_NAMES[j]]]=1
+
+                #all_bin_preds.append(bin_pred_list)
+                avg_acc=avg_acc+accuracy_score(np.array(bin_target),np.array(bin_pred_list),normalize=True)
+                pred_label="|".join(pred_label_list)
+                #print(pred_label)
+                row=[fname,pred_label]
+                csv_writer.writerow(row)
+
+        done=int(((i+1)/len(labels))*100)
+        #sys.stdout.write("\r% of files done processing: "+str(done))
+
+    avg_acc=avg_acc/len(data_files)
+    #avg_acc=jaccard_similarity_score(np.array(all_targets),np.array(all_bin_preds),normalize=True)
+    #print("\n\nResults saved in",output_file_name)
+    print("Average accuracy is: %.2f" %(avg_acc*100))
+    total_time_exec=int(time.time()-start_time)//60
+    return avg_acc*100
+    #print("\nTotal Time taken: "+str(total_time_exec)+" minutes")
+
+if __name__ == "__main__":
+    sys.exit(main())
